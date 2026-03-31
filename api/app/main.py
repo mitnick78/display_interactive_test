8fc7298ea4f9eb8212a6deb8fee82c30ea58ad79
mitnick78 <mchristophe7893@gmail.com>
2026-03-31 18:22:32 +0200

    feat: initialize front-end with React, Vite - Added React and Vite configuration files. - Created main entry point for the React application. - Set up TypeScript configuration for app and node environments. - Initialized Python project

---
+++ api/app/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.routes.health import router as health_router 
+
+
+app = FastAPI(
+    title="UGO API",
+    description="API de gestion des customers et de leurs commandes",
+    version="1.0.0",
+)
+
+# CORS API 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",      
+        "http://127.0.0.1:5173",           
+        "http://ugo_front:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+app.include_router(health_router)
+
+
+
